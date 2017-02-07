9a904b8af3ec76dfa22e9a55e9a88d84f03a614f
Rafael Dalprá <thedalpra@gmail.com>
2017-02-07 10:49:27 -0200

    Renamed test file

--- test.sh
+++ test.sh
@@ -3,3 +3,5 @@ docker run --rm -v "$PWD":/usr/src/app -w /usr/src/app ruby:2.1 ruby unit_tests.
 cd ../geofilter
 docker run --rm -v "$PWD":/usr/src/app -w /usr/src/app ruby:2.1 ruby unit_tests.rb
 cd ..
+
+
